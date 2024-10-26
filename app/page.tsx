cccef436bb99a0e92e1568c051cf848191c56f54
小川智也 <ogawatomoya@ogawatomoyanoMacBook-Pro.local>
2024-10-26 19:41:19 +0900

    ホーム画面の追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,50 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/Header";
 import { Video } from "@/types/video";
-
-const videos: Video[] = [
-  {
-    id: "1",
-    title: "【黒3DIO】2023年お疲れ様♡よしよしASMR【健屋花那/にじさんじ】",
-    thumbnail: "/thumb1.jpg",
-    channelId: "健屋花那【にじさんじ】KanaSukoya",
-    channelName: "健屋花那【にじさんじ】KanaSukoya",
-    views: "196万",
-    timestamp: "2 days ago",
-  },
-  {
-    id: "2",
-    title: "イケメンかるた【火威青 】#hololiveDEV_IS #ReGLOSS",
-    thumbnail: "/thumb2.jpg",
-    channelId: "Ao Ch. 火威青 ‐ ReGLOSS",
-    channelName: "Ao Ch. 火威青 ‐ ReGLOSS",
-    views: "53万",
-    timestamp: "1 week ago",
-  },
-  {
-    id: "3",
-    title: "【クイズ企画】私たちのイメージが丸裸に！？【#ReGLOSS印象調査】",
-    thumbnail: "/thumb3.jpg",
-    channelId: "hololive DEV_IS",
-    channelName: "hololive DEV_IS",
-    views: "321万",
-    timestamp: "3 days ago",
-  },
-];
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [videos, setVideos] = useState<Video[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await axios.post(
+          "https://devesion.main.jp/jphacks/api/main.php",
+          {
+            get_movie_list: "",
+          },
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        setVideos(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    };
+
+    fetchVideos();
+  }, []);
+  const handleNavigation = (video: Video) => {
+    const params = new URLSearchParams();
+    params.append("title", video.title);
+    params.append("thumbnail_url", video.thumbnail_url);
+    params.append("movie_url", video.movie_url);
+    params.append("tags", video.tags);
+    params.append("language", video.language);
+    router.push(`/video/${video.movie_id}?${params.toString()}`);
+  };
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
@@ -46,30 +54,46 @@ export default function Home() {
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
           {videos.map((video) => (
-            <Link href={`/video/${video.id}`} key={video.id} className="group">
-              <div key={video.id} className="group cursor-pointer">
+            <button
+              onClick={() => handleNavigation(video)}
+              key={video.movie_id}
+              className="group"
+            >
+              <div key={video.movie_id} className="group cursor-pointer">
                 <div className="bg-card rounded-lg overflow-hidden shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105">
                   <div className="aspect-video relative">
-                    <Image
-                      src={video.thumbnail}
-                      alt={video.title}
-                      layout="fill"
-                      objectFit="cover"
-                      className="transition-transform group-hover:scale-110"
-                    />
+                    <p>{video.thumbnail_url}</p>
+                    {video.thumbnail_url !== "" &&
+                    video.thumbnail_url !== "1.png" ? (
+                      <Image
+                        src={video.thumbnail_url}
+                        alt={video.title}
+                        layout="fill"
+                        objectFit="cover"
+                        className="transition-transform group-hover:scale-110"
+                      />
+                    ) : (
+                      <Image
+                        src={"/thumb1.jpg"}
+                        alt={video.title}
+                        layout="fill"
+                        objectFit="cover"
+                        className="transition-transform group-hover:scale-110"
+                      />
+                    )}
                   </div>
                   <div className="p-4">
                     <h2 className="text-lg font-semibold line-clamp-2 text-foreground">
                       {video.title}
                     </h2>
-                    <p className="text-sm text-gray-500">{video.views}回再生</p>
+                    {/* <p className="text-sm text-gray-500">{video.views}回再生</p> */}
                   </div>
                 </div>
               </div>
-            </Link>
+            </button>
           ))}
         </div>
-        <h1 className="text-2xl font-bold mb-6 text-foreground mt-12">
+        {/* <h1 className="text-2xl font-bold mb-6 text-foreground mt-12">
           新着動画
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
@@ -96,7 +120,7 @@ export default function Home() {
               </div>
             </Link>
           ))}
-        </div>
+        </div> */}
       </main>
     </div>
   );
